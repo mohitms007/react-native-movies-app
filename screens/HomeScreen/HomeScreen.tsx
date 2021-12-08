702aeb22090bc8c85eaf016911654768c6d37b4b
mohitms007 <mohitms015@gmail.com>
2021-12-08 10:14:48 +0530

    added movieDetails

---
+++ screens/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { StyleSheet } from 'react-native';
+
+import EditScreenInfo from '../../components/EditScreenInfo';
+import { Text, View } from '../../components/Themed';
+import { RootTabScreenProps } from '../../types';
+
+export default function HomeScreen({ navigation }: RootTabScreenProps<'Home'>) {
+  return (
+      <View style{styles.container}>
+      </View>
+  );
+}
